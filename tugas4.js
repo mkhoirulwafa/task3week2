a3f0d9cf8dfb62419d9d558410b26b5e0e5db148
Muhammad Khoirul Wafa <57463595+mkhoirulwafa@users.noreply.github.com>
2020-09-16 08:36:18 +0700

    update tugas4

--- tugas4.js
+++ tugas4.js
@@ -1,17 +1,19 @@
+const container = document.querySelector('.container')
+
 fetch("http://jsonplaceholder.typicode.com/users")
     .then(response => {
         return response.json()
     })
     .then(data => {
-        console.log(data)
+        // console.log(data)
         const hasil = data.map(el => {
             return el.name
         })
-        const hasilAkhir = hasil.map(el =>{
-            return `<p> Nama : ${el}</p>`
-        }).join('')
+        for (let i = 0; i < hasil.length; i++) {
+             //tampil di page html
+            container.insertAdjacentHTML('afterbegin', `<p> Nama : ${hasil[i]}</p>`)      
+        }
         console.log(hasil) //tampil di console
-        const container = document.querySelector('.container')
-        container.insertAdjacentHTML('afterbegin', hasilAkhir) //tampil di page html
     })
     .catch(err => console.log(err))
+    // .then(data => console.log(data))
